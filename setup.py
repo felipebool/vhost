a03cdb19bf70f4369d526782a838cf8bba1f7448
Felipe Lopes <bolzin@gmail.com>
2018-04-08 18:12:42 -0300

    :art: few changes to generate package

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@
 
 from setuptools import setup
 
-setup(name='vhost',
+setup(
+    name='vhost',
     version='0.1.0',
     description='Tool for help creating virtual hosts',
     author='Felipe Lopes',
     author_email='bolzin@gmail.com',
     url='https://www.github.com/felipebool/vhost',
+    download_url='https://www.github.com/felipebool/vhost/archive/0.1.0.tar.gz',
     packages=['vhost'],
     license='MIT',
     entry_points={
         'console_scripts': [
             'vhost = vhost.__main__:main'
         ]
-    })
+    }
+)
 
