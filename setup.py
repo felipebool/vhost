b8c9ab2d2fbe2ab9d7fff9ef90bafcaf5bed30cc
Felipe Lopes <bolzin@gmail.com>
2018-04-08 17:56:07 -0300

    :memo: bump version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='vhost',
-    version='0.1',
+    version='0.1.0',
     description='Tool for help creating virtual hosts',
     author='Felipe Lopes',
     author_email='bolzin@gmail.com',
